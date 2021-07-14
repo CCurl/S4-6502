33fec5e66af5b310021e5936c5704d8376f8c7bf
Chris Curl <curlbud-github.com>
2021-07-13 21:40:06 -0400

    Added opcodes

--- 6502Emu/6502.h
+++ 6502Emu/6502.h
@@ -1,11 +1,24 @@
 #pragma once
 
+#define OP_BEQ 0xF0
+#define OP_BNE 0xD0
+
+#define OP_JMP  0x4C
+#define OP_JMPI 0x6C
+
 #define OP_BRK 0x00
 #define OP_JSR 0x20
 #define OP_PHA 0x48
 #define OP_RTS 0x60
 #define OP_PLA 0x68
 #define OP_NOP 0xEA
+
+#define OP_TAX 0xAA
+#define OP_TXA 0x8A
+#define OP_TAY 0xA8
+#define OP_TYA 0x98
+#define OP_TSX 0xBA
+#define OP_TXS 0x9A
 
 #define LDA_IMM 0xA9
 #define LDA_ZP  0xA5
@@ -16,17 +29,29 @@
 #define LDA_INX 0xA1
 #define LDA_INY 0xB1
 
-#define STA_ZP  0x85
+#define LDX_IMM 0xA2
+#define LDX_ZP  0xA6
+#define LDX_ZPY	0xB6
+#define LDX_ABS 0xAE
+#define LDX_ABY	0xBE
 
+#define LDY_IMM 0xA0
+#define LDY_ZP  0xA4
+#define LDY_ZPX	0xB4
+#define LDY_ABS 0xAC
+#define LDY_ABX	0xBC
+
+#define STA_ZP  0x85
 
 #define IMM 1
 #define ZP  2
 #define ZPX 3
-#define ABS 4
-#define ABX 5
-#define ABY 6
-#define INX 7
-#define INY 8
+#define ZPY 4
+#define ABS 5
+#define ABX 6
+#define ABY 7
+#define INX 8
+#define INY 9
 
 
 #define FLAG_C (0x01)
@@ -37,3 +62,6 @@
 #define FLAG_B (0x20)
 #define FLAG_V (0x40)
 #define FLAG_N (0x80)
+
+
+
--- 6502Emu/6502Emu.cpp
+++ 6502Emu/6502Emu.cpp
@@ -8,7 +8,7 @@ typedef unsigned char byte;
 typedef unsigned short ushort;
 
 byte mem[65536];
-long cycles, lastPage;
+long cycles, lastPage, t1, t2, t3;
 typedef struct {
     byte a, x, y, p;
     byte sp;
@@ -65,8 +65,8 @@ void writeByte(ushort addr, byte val) {
 void push(byte val) { mem[STK+(--SP)] = val; }
 byte pop() { return mem[STK+(SP++)]; }
 
-void syncFlags(int p2s) {
-    if (p2s) {
+void syncFlags(int fromP) {
+    if (fromP) {
         sys.flags.c = (FLAGS & FLAG_C) ? 1 : 0;
         sys.flags.z = (FLAGS & FLAG_Z) ? 1 : 0;
         sys.flags.i = (FLAGS & FLAG_I) ? 1 : 0;
@@ -77,7 +77,7 @@ void syncFlags(int p2s) {
         sys.flags.n = (FLAGS & FLAG_N) ? 1 : 0;
         return;
     }
-    sys.p = 0;
+    FLAGS = 0;
     if (sys.flags.c) { sys.p |= FLAG_C; }
     if (sys.flags.z) { sys.p |= FLAG_Z; }
     if (sys.flags.i) { sys.p |= FLAG_I; }
@@ -100,25 +100,63 @@ void init() {
 }
 
 ushort wordAt(ushort addr) {
-    return readByte(addr) | (readByte(addr+1) << 8);
+    return mem[addr] | (mem[addr+1] << 8);
 }
+
+inline byte xB(long x) { return (x & 0xFF); }
 
 void doLDA(int addrMode) {
     switch (addrMode) {
-    case IMM: REG_A = mem[PC];                 P1; C(2); break;
-    case ZP:  REG_A = mem[mem[PC]];            P1; C(3); break;
-    case ZPX: REG_A = mem[mem[PC] + REG_X];    P1; C(4); break;
-    case ABS: REG_A = mem[wordAt(PC)];         P2; C(4); break;
-    case ABX: REG_A = mem[wordAt(PC) + REG_X]; P2; C(4); break;
-    case ABY: REG_A = mem[wordAt(PC) + REG_Y]; P2; C(4); break;
-    case INX: REG_A = mem[wordAt(PC) + REG_X]; P2; C(4); break;
-    case INY: REG_A = mem[wordAt(PC) + REG_Y]; P2; C(4); break;
-    default:
-        break;
+    case IMM: REG_A = mem[PC];                   t2 = 1; t3 = 2; break;
+    case ZP:  t1 = mem[PC];                      t2 = 1; t3 = 3; break;
+    case ZPX: t1 = xB(mem[PC] + REG_Y);          t2 = 1; t3 = 4; break;
+    case ABS: t1 = wordAt(PC);                   t2 = 2; t3 = 4; break;
+    case ABX: t1 = wordAt(PC) + REG_X;           t2 = 2; t3 = 4; break;
+    case ABY: t1 = wordAt(PC) + REG_Y;           t2 = 2; t3 = 4; break;
+    case INX: t1 = wordAt(xB(mem[PC] + REG_X));  t2 = 2; t3 = 6; break;
+    case INY: t1 = wordAt(mem[PC]) + REG_Y;      t2 = 2; t3 = 5; break;
+    default: break;
     }
+    if (addrMode != IMM) { REG_A = mem[t1]; }
+    PC += (ushort)t2;
+    cycles += t3;
     sys.flags.z = (REG_A) ? 0 : 1;
     sys.flags.n = (REG_A <= 0x7F) ? 0 : 1;
-    syncFlags(0);
+    //syncFlags(0);
+}
+
+void doLDX(int addrMode) {
+    switch (addrMode) {
+    case IMM: REG_X = mem[PC];                   t2 = 1; t3 = 2; break;
+    case ZP:  t1 = mem[PC];                      t2 = 1; t3 = 3; break;
+    case ZPY: t1 = xB(mem[PC] + REG_Y);          t2 = 1; t3 = 4; break;
+    case ABS: t1 = wordAt(PC);                   t2 = 2; t3 = 4; break;
+    case ABY: t1 = wordAt(PC) + REG_Y;           t2 = 2; t3 = 4; break;
+    default: break;
+    }
+    if (addrMode != IMM) { REG_X = mem[t1]; }
+    PC += (ushort)t2;
+    cycles += t3;
+    sys.flags.z = (REG_X) ? 0 : 1;
+    sys.flags.n = (REG_X <= 0x7F) ? 0 : 1;
+    //syncFlags(0);
+}
+
+void doLDY(int addrMode) {
+    switch (addrMode) {
+    case IMM: REG_Y = mem[PC];                   t2 = 1; t3 = 2; break;
+    case ZP:  t1 = mem[PC];                      t2 = 1; t3 = 3; break;
+    case ZPY: t1 = xB(mem[PC] + REG_X);          t2 = 1; t3 = 4; break;
+    case ABS: t1 = wordAt(PC);                   t2 = 2; t3 = 4; break;
+    case ABY: t1 = wordAt(PC) + REG_X;           t2 = 2; t3 = 4; break;
+    default: break;
+    }
+    if (addrMode != IMM) { REG_Y = mem[t1]; }
+    PC += (ushort)t2;
+    cycles += t3;
+    sys.flags.z = (REG_Y) ? 0 : 1;
+    sys.flags.n = (REG_Y <= 0x7F) ? 0 : 1;
+    //syncFlags(0);
 }
 
 int run(ushort pc, long maxCycles) {
@@ -146,8 +184,41 @@ int run(ushort pc, long maxCycles) {
         case LDA_ZP:  doLDA(ZP);  break;
         case LDA_ZPX: doLDA(ZPX); break;
         case LDA_ABS: doLDA(ABS); break;
+        case LDA_ABX: doLDA(ABX); break;
+        case LDA_ABY: doLDA(ABY); break;
+        case LDA_INX: doLDA(INX); break;
+        case LDA_INY: doLDA(INY); break;
+
+        case LDX_IMM: doLDX(IMM); break;
+        case LDX_ZP:  doLDX(ZP);  break;
+        case LDX_ZPY: doLDX(ZPY); break;
+        case LDX_ABS: doLDX(ABS); break;
+        case LDX_ABY: doLDX(ABY); break;
+
+        case LDY_IMM: doLDY(IMM); break;
+        case LDY_ZP:  doLDY(ZP);  break;
+        case LDY_ZPX: doLDY(ZPY); break;
+        case LDY_ABS: doLDY(ABS); break;
+        case LDY_ABX: doLDY(ABY); break;
+
+        case OP_BEQ: PC += (sys.flags.z) ? mem[PC] : 1;
+            cycles += 2;
+            break;
+        case OP_BNE: PC += (sys.flags.z == 0) ? mem[PC] : 1;
+            cycles += 2;
+            break;
+
+        case OP_JMP: PC = wordAt(PC);          cycles += 3; break;
+        case OP_JMPI: PC = wordAt(wordAt(PC)); cycles += 5; break;
 
         case STA_ZP:  mem[mem[PC]] = REG_A;         P1; C(3); break;
+
+        case OP_TAX: REG_X = REG_A; cycles += 2; break;
+        case OP_TXA: REG_A = REG_X; cycles += 2; break;
+        case OP_TAY: REG_Y = REG_A; cycles += 2; break;
+        case OP_TYA: REG_A = REG_Y; cycles += 2; break;
+        case OP_TSX: SP = REG_X; cycles += 2; break;
+        case OP_TXS: REG_X = SP; cycles += 2; break;
 
         case OP_NOP: break;
         default: printf("-%d:%d-", PC-1, ir);
